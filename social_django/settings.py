99bf2709799070cf322f2adb29efc5bdea958fd8
Campero9813 <60483394+Campero9813@users.noreply.github.com>
2022-01-17 11:26:30 -0600

    update settings

--- social_django/settings.py
+++ social_django/settings.py
@@ -29,6 +29,73 @@ DEBUG = True
 ALLOWED_HOSTS = ['cardex.tescacorporation.com','34.217.74.14' , 'localhost']
 
 
+#AWS Configuration S3
+
+#[ dafault ] 
+#aws_access_key_id  = 'AKIA3YVW5CDSB456QEBF'
+#aws_secret_access_key  =  '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'
+
+#s3 = boto3.resource('s3',
+#         aws_access_key_id='AKIA3YVW5CDSB456QEBF',
+#         aws_secret_access_key='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')
+
+# S3 Bucket Configurations  
+
+AWS_ACCESS_KEY_ID= 'AKIA5E6VJQ4FRDYVXOIH'  
+
+AWS_SECRET_ACCESS_KEY= '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'
+
+AWS_STORAGE_BUCKET_NAME= 'cardexbuckettesca'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_REGION_NAME = 'us-west-2' #change to your region
+
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+AWS_S3_FILE_OVERWRITE = False  
+
+AWS_DEFAULT_ACL = None  
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'   
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATICFILES_LOCATION = 'static'
+#STATICFILES_STORAGE = 'django_lender.custom_storages.StaticStorage'
+STATIC_URL = 'https://{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+
+MEDIAFILES_LOCATION = 'media'
+#DEFAULT_FILE_STORAGE = 'django_lender.custom_storages.MediaStorage'
+MEDIA_URL = 'htts://{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+
+
+#STATIC_URL = '/static/'
+
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+#Nunca comentar estas dos lineas
+LOGIN_REDIRECT_URL = 'feed'
+LOGIN_URL = 'login'
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
